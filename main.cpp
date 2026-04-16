bbe7f8267eabae99a0e79dc3b0f14e74963da06e
Saankhya <71884422+SaankLeo@users.noreply.github.com>
2026-04-16 10:49:57 +0530

    added feature to recieve input and put them in code and memory arrays

--- main.cpp
+++ main.cpp
@@ -1,37 +1,119 @@
 #include "cpu.h"
+#include <vector>
+#include <string>
+#include <sstream>
+#include <cctype>
 using namespace std;
 
 int main() {
+     
+    array<int,128> codeMemory = {0}; // Stores instructions like LOAD, ADD, etc.
+    array<int,128> dataMemory = {0}; // Stores actual values like 5, 3, 10, etc
+    vector<int> program;
+    
+    int dataIndex = 0;    // Tracks the next empty slot in data memory
 
-    array<int,256> memory = {0};
+    cout << "Enter instructions\n";
+    cout << "Examples:\n";
+    cout << "r0 = 5\n";
+    cout << "r1 = 3\n";
+    cout << "r2 = r0 + r1\n";
+    cout << "Type run to execute\n\n";
 
-    int program[] = {
-        
+    string line;  // Stores the entire line the user types
 
-    // LOAD R0 <- 5
-    1, 0, 5, 0,
+    while (true) {
+        cout << "cpu> ";
+        getline(cin, line); // Reads full line like: r2 = r0 + r1
 
-    // LOAD R1 <- 3
-    1, 1, 3, 0,
+    if (line == "run") {  // Stop taking input if user types run
+            break;
+        }
+    
+    stringstream ss(line);   // Lets us break the line into smaller words
 
-    // R2 = R0 + R1
-    2, 2, 0, 1,
+    string left, equalSign, first, op, second; // Variables for different parts of the instruction
 
-    // R3 = R2 - R1
-    3, 3, 2, 1,
+     // Example:
+        // r2 = r0 + r1
+        // left = "r2"
+        // equalSign = "="
+        // first = "r0"
+    
+    ss >> left >> equalSign >> first;
+    // Reads first 3 parts of the instruction
+    // Example: r2 = r0
+    // left = "r2"
+    // equalSign = "="
+    // first = "r0"
 
-    // R4 = R3 * R1
-    4, 4, 3, 1,
+    int dest = stoi(left.substr(1));
+    // Removes the 'r' from "r2"
+    // Converts "2" into integer 2
 
-    // HALT
-    5, 0, 0, 0
+    if (isdigit(first[0])) {
+        // Checks if first is a number
+        // Example: "5"
 
-    };
+        int value = stoi(first);
+        // Converts string "5" into integer 5
 
-    int size = sizeof(program)/sizeof(program[0]); //each entity might not be of one byte
+        dataMemory[dataIndex] = value;
+        // Stores value in next free data memory slot
 
-    for(int i=0;i<size;i++)
-        memory[i] = program[i];
+        program.push_back(LOAD);
+        // Opcode for LOAD instruction
 
-    runCPU(memory, size);
+        program.push_back(dest);
+        // Destination register
+
+        program.push_back(dataIndex);
+        // Location in data memory where value is stored
+
+        program.push_back(0);
+        // Unused field
+
+        dataIndex++;
+        // Move to next free data memory slot
+    }
+    else {
+        // Means this is something like:
+        // r2 = r0 + r1
+
+        ss >> op >> second;
+        // Reads remaining parts
+        // op = "+"
+        // second = "r1"
+
+        int src1 = stoi(first.substr(1));
+        // Converts "r0" into 0
+
+        int src2 = stoi(second.substr(1));
+        // Converts "r1" into 1
+
+        int opcode;
+        // Stores which operation to perform
+
+        if (op == "+")
+            opcode = ADD;
+        else if (op == "-")
+            opcode = SUB;
+        else
+            opcode = MUL;
+        // Decide which opcode to use
+
+        program.push_back(opcode);
+        // Store opcode
+
+        program.push_back(dest);
+        // Store destination register
+
+        program.push_back(src1);
+        // Store first source register
+
+        program.push_back(src2);
+        // Store second source register
+}
+    
+    
 }
